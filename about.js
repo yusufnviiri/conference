aa87d539202c7bd4a7c06746520d6dce65b921d4
yusufnviiri <yusufnviiri@gmail.com>
2022-04-15 21:39:51 +0300

    Create files for the capstone project

---
+++ about.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+const showMenu = document.querySelector('#burger');
+const hideMenu = document.querySelector('#hideMenu');
+const popup = document.querySelector('.mobile-menu');
+
+function openMenu() {
+  popup.style.display = 'flex';
+}
+function closeMenu() {
+  popup.style.top = '-100%';
+  popup.style.width = '50%';
+  window.setTimeout(() => {
+    window.location.reload();
+  }, 5000);
+}
